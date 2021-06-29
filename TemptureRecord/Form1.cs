e4ca309e6ed3bd5fc445d57306dcb1f9a9de6a03
sai wanza <q020385791@gmail.com>
2021-06-29 12:08:31 +0800

    隱藏瀏覽器和小黑窗

--- TemptureRecord/Form1.cs
+++ TemptureRecord/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -24,11 +25,32 @@ namespace TemptureRecord
         private int NameIndex = 0;
         private void btnsign_Click(object sender, EventArgs e)
         {
-            //google Form select dropdown
+            string Msg = "";
+            if (string.IsNullOrEmpty(cblistName.Text)|| cblistName.Text=="選擇")
+            {
+                Msg += "請輸入人員"+Environment.NewLine;
+            }
+            if (string.IsNullOrEmpty(txtTempture.Text.Trim()))
+            {
+                Msg += "請輸入體溫" + Environment.NewLine;
+            }
 
-            IWebDriver driver = new ChromeDriver();
+            if (Msg!="")
+            {
+                MessageBox.Show(Msg);
+                return;
+            }
+
+            //google Form select dropdown
+            btnsign.Enabled = false;
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true; //隱藏cmd
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless"); //隱藏瀏覽器
+            IWebDriver driver = new ChromeDriver(driverService, chromeOptions);
+          
             driver.Navigate().GoToUrl("");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             //下拉選單主框
             By selectXPath = By.XPath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]");
             //點擊主框
@@ -44,10 +66,15 @@ namespace TemptureRecord
             //填入輸入框
             driver.FindElement(By.XPath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")).SendKeys(txtTempture.Text);
 
-
             //提交
             driver.FindElement(By.XPath("//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div/div")).Click();
+            
+            Thread.Sleep(1000);
+            driver.Close();
+            driver.Dispose();
 
+            MessageBox.Show("紀錄完成");
+            this.Dispose();
         }
 
         private void Form1_Load(object sender, EventArgs e)
